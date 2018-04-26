876294b986a0048854511ad5641438d369a00ea6
sspatari <spatari.stanislav@gmail.com>
2018-04-26 19:39:53 +0200

    Add boiler plate code to index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,42 @@
 import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
 import config from './config';
 
 let app = express();
 
+if(app.get('env') === 'development') var dev = true;
+// log if in dev mode
+if(dev) app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//==================================================
+// Routes
+//==================================================
+
+// handle 404
+app.use((req, res, next) => {
+  let err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+if(dev) {
+  app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send();
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send();
+});
+
+
 app.listen(config.port, () => {
-  console.log(`Application is running on port: ${config.port}`);
+  console.log(`Listening at http://localhost:${config.port} in ${app.get('env')} mode`);
 });
